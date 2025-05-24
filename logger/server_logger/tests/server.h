cfdd116afe37f164f2f4d26c6ea86297844543e7
zggff <gigamaximwachau@gmail.com>
2025-05-24 20:27:35 +0300

    add server tests

--- logger/server_logger/tests/server.h
+++ logger/server_logger/tests/server.h
@@ -5,30 +5,29 @@
 #ifndef MP_OS_SERVER_H
 #define MP_OS_SERVER_H
 
-//#include <crow.h>
+#include <crow.h>
 #include <unordered_map>
 #include <logger.h>
-//#include <mutex>
+// #include <mutex>
 #include <shared_mutex>
 
-class server
-{
-    //crow::SimpleApp app;
+class server {
+    crow::SimpleApp app;
 
-    std::unordered_map<int, std::unordered_map<logger::severity, std::pair<std::string, bool>>> _streams;
+    std::unordered_map<
+        int, std::unordered_map<logger::severity, std::pair<std::string, bool>>>
+        _streams;
 
     std::shared_mutex _mut;
 
-public:
-
+  public:
     explicit server(uint16_t port = 9200);
 
-    server(const server&) = delete;
-    server& operator=(const server&) = delete;
-    server(server&&) noexcept = delete;
-    server& operator=(server&&) noexcept = delete;
+    server(const server &) = delete;
+    server &operator=(const server &) = delete;
+    server(server &&) noexcept = delete;
+    server &operator=(server &&) noexcept = delete;
     ~server() noexcept = default;
 };
 
-
-#endif //MP_OS_SERVER_H
+#endif // MP_OS_SERVER_H
